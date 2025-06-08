55aa5d06465c452e849f28815edc4882c1070f6d
adarshj12 <adarshjayadevan12@gmail.com>
2025-06-08 21:52:32 +0530

    message update 0

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/',(req,res)=>{
-    res.json({message:'Server Application'});
+    res.json({message:'Server App'});
 })
 
 app.listen(3000,()=>{
