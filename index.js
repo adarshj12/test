7ea6eec884f0d4e37b69678fde27ae55b920fd1b
adarshj12 <adarshjayadevan12@gmail.com>
2025-06-09 21:38:21 +0530

    commit updated 2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/',(req,res)=>{
-    res.json({message:'Server App 5'});
+    res.json({message:'Server App 6'});
 })
 
 app.listen(3000,()=>{
